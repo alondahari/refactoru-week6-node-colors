9912d459e2fcdf60c6d3d3333fab81062fd5ce31
Alon Dahari <climbinghobo@gmail.com>
2015-02-16 11:54:21 -0700

    add islight and utility file

---
+++ color-utils.js
@@ -0,0 +1,7 @@
+
+
+module.exports = {
+	luminosity: function (r, g, b) {
+		return  0.2126*r + 0.7152*g + 0.0722*b;
+	}
+};
--- islight.js
+++ islight.js
@@ -4,4 +4,6 @@ var r = process.argv[2];
 var g = process.argv[3];
 var b = process.argv[4];
 
-console.log(utils.luminosity(r, g, b));
+var isLight = utils.luminosity(r, g, b) > 155 ? 'light' : 'dark';
+
+console.log(isLight);
